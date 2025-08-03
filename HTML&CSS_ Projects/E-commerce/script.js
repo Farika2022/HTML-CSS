d6e0c890d312beca0755e6b63a5ffb562034de88
Farika2022 <farikamagjabeen2022@gmail.com>
2025-08-03 20:08:08 +0200

    added cart and payment pages to E-commerce project

--- HTML&CSS_ Projects/E-commerce/script.js
+++ HTML&CSS_ Projects/E-commerce/script.js
@@ -11,3 +11,20 @@ function closeNavbar()
 sidenavbar.style.left="-60%"
 
 }
+
+ function addcart(name, image,price) {
+    const newItem = { name, image,price };
+     // Get existing items or start with an empty array
+    const existingCart = JSON.parse(localStorage.getItem('cartItems')) || [];
+    
+     // Add the new item to the array
+    existingCart.push(newItem);
+
+    // Save updated cart back to localStorage
+    localStorage.setItem('cartItems', JSON.stringify(existingCart));
+
+    // Redirect to cart
+    window.location.href = 'cart.html';
+  }
+   
+
